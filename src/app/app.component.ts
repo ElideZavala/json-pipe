14d3f45144df0386e109eed56ea5302c61e7346c
Elide Zavala <zavalavinagreelide@gmail.com>
2023-01-15 12:25:10 -0600

    json pipe

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Libro } from './libro'
+import { Escritor } from './escritor'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'json-pipe';
+
+  libro1: Libro = new Libro('Mucho ruido y pocas nueces', 'Comedia');
+  libro2: Libro = new Libro('Romeo y Julieta', 'Drama');
+  librosArray: Libro[] = [this.libro1, this.libro2];
+
+  escritor = new Escritor(1, 'Wiliam Shakespeare', new Date(1564, 3, 26), this.librosArray)
 }
---
+++ src/app/libro.ts
@@ -0,0 +1,9 @@
+export class Libro {
+
+  constructor(
+    public titulo: string,
+    public tematica: string
+  ) {
+
+  }
+}
